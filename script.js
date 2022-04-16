e26903e4961d7259cc1f34ab7698ac418b8aa604
Luis Almeida <lf.mc.almeida@poli.ufrj.br>
2022-04-16 03:05:31 -0300

    completo-sem-bonus

--- script.js
+++ script.js
@@ -1,21 +1,99 @@
-const trataErro = erro => erro.response;
-const trataAcerto = acerto => acerto;
+let mensagens = [];
+let pegou = {};
+let naoPegou ={};
+let usuario = {};
+let conectou ={};
+let tokenMensagens = 0;
+let renovaStatus =0;
+let texto = 0;
 
-function manterConectado(usuario) {
+const renovaChat = renova => {
+    renovaStatus = renova.status;
+    clearInterval(tokenMensagens);
+    pegarMensagens();
+}
+const trataErro = erro => naoPegou= erro.response;
+const mantemConectado = acerto => conectou = acerto;
+const trataAcerto = acerto =>{
+    pegou = acerto;
+    tokenMensagens = setInterval(pegarMensagens,3000) ;
+    manterConectado();
+};
+const pegaDados = resposta =>{
+    mensagens = resposta.data;
+    setTimeout(renderizaMensagens, 300);
+} ;
+
+
+function pegarMensagens() {
+    const promessa = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages",usuario);
+    promessa.then(pegaDados);
+};
+
+function enviarMensagem(){
+    texto = document.querySelector(".enviamensagem").value;
+    const objetoMensagem = {
+        from: usuario.name,
+        to: "Todos",
+        text: texto,
+        type: "message"
+    };
+
+    const promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages",objetoMensagem);
+    promessa.then(renovaChat);
+    promessa.catch(trataErro);
+    if(naoPegou){
+        console.log(naoPegou);
+    }
+    document.querySelector(".enviamensagem").value = '';
+}
+
+function renderizaMensagens(){
+    const documento = document.querySelector(".boxmensagens");
+    documento.innerHTML = ``;
+    console.log('entrei aqui');
+    console.log(mensagens);
+    for (let index = 0; index < mensagens.length; index++) {
+        if(mensagens[index].type ==='status'){
+            documento.innerHTML += `
+            <div class="mensagens login">
+                <h1>(${mensagens[index].time}) <span>${mensagens[index].from} </span>${mensagens[index].text} </h1>
+            </div>`
+        }
+
+        if(mensagens[index].type ==='message'){
+            documento.innerHTML += `
+            <div class="mensagens publica">
+                <h1>(${mensagens[index].time}) <span>${mensagens[index].from}</span> para <span>${mensagens[index].to}</span>: ${mensagens[index].text} </h1>
+            </div>`
+        }
+
+        if(mensagens[index].type ==='private_message'){
+            documento.innerHTML += `
+            <div class="mensagens privada">
+                <h1>(${mensagens[index].time}) <span>${mensagens[index].from}</span> resevardamente para <span>${mensagens[index].to}</span>: ${mensagens[index].text} </h1>
+            </div>`
+        }
+        
+        documento.scrollIntoView(false);
+    }
+
+}
+
+function manterConectado() {
     let promessa = {};
     let acerto = {};
     let erro = {};
     const conectado = setInterval(() => {
         promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", usuario);
-        acerto = promessa.then(trataAcerto);
+        acerto = promessa.then(mantemConectado);
         erro = promessa.catch(trataErro);
-    console.log(acerto.status);
     },4000);
-    
 
-    if(erro.status === 400){
+    if(naoPegou.status === 400){
         alert("usuario não logado relogue o usuario");
         clearInterval(conectado);
+        clearInterval(tokenMensagens);
     }
     
 }
@@ -26,20 +104,13 @@ function sair(conectado){
 
 function logar(){
     let nome = prompt("Digite seu usuario");
-    let usuario ={
+    usuario ={
         name: nome
     } ;
     let promessa = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usuario);
-    let acerto = promessa.then(trataAcerto);
-    let erro = promessa.catch(trataErro);
-    console.log(erro);
-    console.log(acerto);
+    promessa.then(trataAcerto);
+    promessa.catch(trataErro);
 
-    if(erro.status == 400){
-        alert("relogue a página e escolha outro usuário.");
-    }
-
-    manterConectado(usuario);
 }
 
 logar();
